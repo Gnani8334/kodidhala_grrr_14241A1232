924be865e8046f42b364e0819c727d6b51dfe785
Gnani8334 <31875032+Gnani8334@users.noreply.github.com>
2017-09-12 13:41:11 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+StudentGroup a=new StudentGroup();
+Scanner s=new Scanner(System.in);
+String Students[]=new.String[n];
+int n=nextInt();
+a.setStudents(Student);
+System.out.println("enter index");
+int index=s.nextInt();
+a.getStudent(index);
+System.out.println("enter the string");
+String abc=s.next();
+System.out.println("enter the index");
+int index1=s.nextInt();
+a.setStudent(abc,index1);
+System.out.print("enter d string u want to insert at d begining");
+String abc1=s.next();
+a.addFirst(abc1);
+System.out.print("enter d string u want to insert at d end");
+String abc2=s.next();
+a.addLast(abc2);
+System.out.println("enter the index");
+int in=s.nextInt();
+System.out.print("enter d string to insert");
+String ab=s.next();
+a.add(abc,in);
+System.out.println("enter d ind where u want to delete");
+int in1=s.nextInt();
+a.remove(in1);
+System.out.println("enter d  string to delete");
+String abcd=s.next();
+a.remove(abcd);
+
+
+
+
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
